cca59362ff6b1e16da624c67f05e6f11c34b6351
Po-Yeh Chen <poyehchen@cs.nycu.edu.tw>
2024-02-18 21:17:48 +0800

    Merge flake.nix.

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,15 +16,26 @@
       url = "github:Aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-ld-rs = {
+      url = "github:nix-community/nix-ld-rs";
+      inputs.nixpkgs.follows = "nixos";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, decl-cachix, devenv, ags, ... }:
+  outputs = { nixpkgs, nixos, home-manager, decl-cachix, devenv, ags, nix-ld-rs, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       devenv' = devenv.outputs.packages.${system}.default;
     in {
       formatter.${system} = pkgs.nixfmt;
+      nixosConfigurations."smb374-nix" = nixos.lib.nixosSystem {
+        system = system;
+        modules = [
+          ./os/configuration.nix
+          { programs.nix-ld.package = nix-ld-rs.packages.${system}.nix-ld-rs; }
+        ];
+      };
       homeConfigurations."poyehchen" =
         home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
