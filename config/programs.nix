1e75360398df2745ee655a67539fd1d1d6dab952
Po-Yeh Chen <poyehchen@cs.nycu.edu.tw>
2024-02-19 19:37:01 +0800

    Add theme for lazygit.

--- config/programs.nix
+++ config/programs.nix
@@ -6,6 +6,7 @@
     ./programs.nix.d/fish.nix
     ./programs.nix.d/foot.nix
     ./programs.nix.d/gpg.nix
+    ./programs.nix.d/lazygit.nix
     ./programs.nix.d/rofi.nix
     ./programs.nix.d/tmux.nix
   ];
@@ -30,7 +31,8 @@
       userName = "Po-Yeh Chen";
       userEmail = "poyehchen@cs.nycu.edu.tw";
     };
-    lazygit.enable = true;
+    imv.enable = true;
+    jq.enable = true;
     lf.enable = true;
     neovim.enable = true;
     ripgrep.enable = true;
--- config/programs.nix.d/gpg.nix
+++ config/programs.nix.d/gpg.nix
@@ -1,6 +1,8 @@
 { ... }: {
   programs.gpg = {
     enable = true;
+    mutableKeys = true;
+    mutableTrust = true;
     publicKeys = [{
       source = ../sources/gpg/pub-0x0A507FC2325D77EA.gpg;
       trust = "ultimate";
